7396af1781b32f568c439f2be90c9e838803afac
jaypierce20147 <jaypierce2014@gmail.com>
2025-09-19 21:42:27 -0700

    Finished prep1 and prep2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        //Get the name of the person and read it to the variable
+
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        //Print the name of the person back to them
+        Console.WriteLine($"Your name is  {last_name}, {first_name} {last_name}.");
+
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        // Ask the user for their percentage grade and convert to int
+        Console.Write("What is your grade percentage? ");
+        string gradePer = Console.ReadLine();
+        int number = int.Parse(gradePer);
+        string letter = "";
+
+
+        //Re assign the letter to the correct number
+        if (number >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+
+        //Print the letter grade
+        Console.WriteLine($"Your grade is {letter}");
+        if (number >= 70)
+        {
+            Console.WriteLine("You pass!");
+        }
+        else
+        {
+            Console.WriteLine("You shall not pass!!!!");
+        }
+
     }
 }
