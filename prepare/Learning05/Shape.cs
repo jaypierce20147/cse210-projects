04a2ff5bf1656f7fc068c96199c89de10b3ece66
jaypierce20147 <jaypierce2014@gmail.com>
2025-11-12 00:06:30 -0800

    Finished W09 prepare with a little help form the sample solution

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,24 @@
+using System;
+
+public abstract class Shape
+{
+    private string _color;
+    public abstract double GetArea();
+
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public  Shape(string color)
+    {
+        _color = color;
+    }
+}
+        
